25b008afaeb8560efa94a3645fff0869400b59ac
Coulter Jones <coulterjones@gmail.com>
2015-12-22 15:58:04 -0500

    Creating new file
    Colors used on MedPage Today's general site.

---
+++ mptColors.R
@@ -0,0 +1,13 @@
+#MedPage Today's Colors
+
+colors = c("#ed671d", "#37b34a","#0484d9", "#00235f", "#cccc06", "#ef3e1b", "#666666", "#d2d2d2")
+
+#ed671d -- Orange
+#37b34a -- Green
+#0484d9 -- Light Blue
+#00235f -- Dark Blue
+#cccc06 -- Yellow
+#ef3e1b -- Red
+#666666 -- Dark Grey
+#d2d2d2 -- Light Grey
+#f1f1f1 -- Very light grey
